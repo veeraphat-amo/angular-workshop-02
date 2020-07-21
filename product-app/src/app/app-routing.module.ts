4889f2da6f7ac06957e77154da63cbf67e53d50f
veeraphat-amo <veeraphat_amo@hotmail.com>
2020-07-21 14:55:55 +0700

    try

--- product-app/src/app/app-routing.module.ts
+++ product-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'list', component: ProductListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
