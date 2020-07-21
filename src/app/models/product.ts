22f47e9a4769e679abf8ac23626039174fe274d4
up1 <somkiat.p@gmail.com>
2020-07-21 20:44:58 +0700

    Update class product

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -2,6 +2,5 @@ export class Product {
   public rating: number;
   public available: boolean;
   public imageUrl: string;
-
   constructor(public code: string, public name: string, public price: number) {}
 }
